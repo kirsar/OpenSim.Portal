f5c79b5eb2ac09810176a17298111df979200bef
kirsar <saron312@yandex.ru>
2018-04-01 12:55:47 +0300

    dummy angular view for list of servers

---
+++ OpenSim.WebServer.App/ClientApp/app/components/servers/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'servers',
+    templateUrl: './servers.component.html'
+})
+export class ServersComponent {
+    public servers: Server[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/server').subscribe(result => {
+            this.servers = result.json() as Server[];
+        }, error => console.error(error));
+    }
+}
+
+interface Server {
+    id: number;
+    name: string;
+    description: string;
+    ownerId: number;
+}
