6ddff5909792b0a3d0ffcc191d76c14d73873cc6
kirsar <saron312@yandex.ru>
2019-05-07 18:40:51 +0300

    hamburger drop down with material

--- Portal/ClientApp/src/app/app.module.ts
+++ Portal/ClientApp/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 import {FlexLayoutModule, FlexAlignDirective} from "@angular/flex-layout";
 
@@ -64,6 +64,7 @@ import { PresentationComponent } from './components/presentation/presentation.co
         BrowserAnimationsModule,
         LayoutModule,
         MatToolbarModule, 
+        MatMenuModule,
         MatButtonModule,
         MatSidenavModule,
         MatIconModule,
